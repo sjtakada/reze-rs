3181b37f516d7d9f468cba7486609eaff87286b4
Toshiaki Takada <toshiaki.takada@gmail.com>
2018-10-22 23:00:45 -0700

    ACL test.

--- common/src/acl.rs
+++ common/src/acl.rs
@@ -12,10 +12,12 @@ use std::collections::hash_map::Entry;
 use std::str::FromStr;
 use std::net::Ipv4Addr;
 use std::net::AddrParseError;
+use std::fmt;
 
 // Type definitions.
 
 // (permit|deny)
+#[derive(PartialEq)]
 pub enum AclPerm {
     Permit,
     Deny
@@ -35,13 +37,12 @@ pub struct AclRule {
 
 // List of ACL rules.
 pub struct Acl {
-    pub name: String,
     rules: Vec<AclRule>,
 }
 
 // Collections of ACLs.
 pub struct AclCollection {
-    acls: HashMap<String, Acl>
+    acls: HashMap<String, Acl>,
 }
 
 // Implementations.
@@ -53,6 +54,12 @@ impl AclPerm {
             AclPerm::Permit => "permit",
             AclPerm::Deny => "deny"
         }
+    }
+}
+
+impl fmt::Debug for AclPerm {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
     }
 }
 
@@ -87,8 +94,8 @@ impl AclRule {
 
 // Acl
 impl Acl {
-    pub fn new(name: String) -> Self {
-        Acl{ name: name, rules: Vec::new() }
+    pub fn new() -> Self {
+        Acl{ rules: Vec::new() }
     }
 
     pub fn add_rule(&mut self, perm: AclPerm, addr: &str) {
@@ -102,14 +109,24 @@ impl Acl {
         }
     }
 
-    pub fn show(&self) {
+    pub fn check(&self, addr: &Ipv4Addr) -> &AclPerm {
         for r in &self.rules {
-            println!("access-list {} {} {}",
-                     self.name, r.perm.to_string(), r.addr.to_string());
+            match r.addr {
+                AclRuleAddr::Addr(a) => {
+                    if a == *addr {
+                        return &r.perm
+                    }
+                }
+                AclRuleAddr::Any => {
+                    return &r.perm
+                }
+            }
         }
+        &AclPerm::Deny
     }
 }
 
+// AclCollection
 impl AclCollection {
     pub fn new() -> Self {
         AclCollection { acls: HashMap::new() }
@@ -118,19 +135,58 @@ impl AclCollection {
     pub fn get_mut(&mut self, name: &str) -> &mut Acl {
         match self.acls.entry(name.to_string()) {
             Entry::Occupied(o) => o.into_mut(),
-            Entry::Vacant(v) => v.insert(Acl::new(String::from(name)))
+            Entry::Vacant(v) => v.insert(Acl::new())
         }
     }
 
-//    pub fn get(&mut self, name: &str) -> &Acl {
-//        match self.acls.entry(name.to_string()) {
-//            Entry::Occupied(o) => o.get(),
-//            Entry::Vacant(v) => ()
-//        }
-//    }
+    pub fn get(&self, name: &str) -> Option<&Acl> {
+        self.acls.get(name)
+    }
+
+    pub fn check(&self, name: &str, addr: &Ipv4Addr) -> &AclPerm {
+        match self.get(name) {
+            Some(r) => &r.check(addr),
+            None => &AclPerm::Deny
+        }
+    }
 
     pub fn show(&self, name: &str) {
+        match self.acls.get(name) {
+            Some(acl) => {
+                for r in &acl.rules {
+                    println!("access-list {} {} {}",
+                             name, r.perm.to_string(), r.addr.to_string());
+                }
+            }
+            None => {
+            }
+        }
     }
 }
 
 // Tests.
+
+#[test]
+pub fn test_acl () {
+    let mut ac = AclCollection::new();
+
+    ac.get_mut("100").add_rule(AclPerm::Permit, "1.1.1.1");
+    ac.get_mut("100").add_rule(AclPerm::Deny, "2.2.2.2");
+    ac.get_mut("100").add_rule(AclPerm::Deny, "3.3.3.3");
+    ac.get_mut("100").add_rule(AclPerm::Deny, "any");
+
+    // XXX unwrap
+    let a = Ipv4Addr::from_str("1.1.1.1").unwrap();
+    assert_eq!(&AclPerm::Permit, ac.check("100", &a));
+
+    let a = Ipv4Addr::from_str("2.2.2.2").unwrap();
+    assert_eq!(&AclPerm::Deny, ac.check("100", &a));
+
+    let a = Ipv4Addr::from_str("3.3.3.3").unwrap();
+    assert_eq!(&AclPerm::Deny, ac.check("100", &a));
+
+    let a = Ipv4Addr::from_str("4.4.4.4").unwrap();
+    assert_eq!(&AclPerm::Deny, ac.check("100", &a));
+
+    assert_eq!(&AclPerm::Deny, ac.check("200", &a));
+}
