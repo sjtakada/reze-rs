bd77ef321dfcba46ae7776560ec3a30c3a455879
Toshiaki Takada <toshiaki.takada@gmail.com>
2019-02-16 09:09:01 -0800

    CliNodeKeyword.

--- rzcli/src/collate.rs
+++ rzcli/src/collate.rs
@@ -5,16 +5,52 @@
 // CLI Match Result and Flag.
 //
 
+use std::fmt;
+
 // Match result.
+#[derive(PartialEq)]
 pub enum MatchResult {
     Failure,
     Success,
 }
 
+impl MatchResult {
+    pub fn to_string(&self) -> &str {
+        match *self {
+            MatchResult::Success => "Match success",
+            MatchResult::Failure => "Match failure",
+        }
+    }
+}
+
+impl fmt::Debug for MatchResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
 // Match flag.
+#[derive(PartialEq)]
 pub enum MatchFlag {
     Full,         // Fully matched.
     Partial,      // Partially matched, still valid.
     Incomplete,   // String incomplete, not valid for execution.
     None,         // Not matched.
+}
+
+impl MatchFlag {
+    pub fn to_string(&self) -> &str {
+        match *self {
+            MatchFlag::Full => "Match full",
+            MatchFlag::Partial => "Match partial",
+            MatchFlag::Incomplete => "Match incomplete",
+            MatchFlag::None => "Match none",
+        }
+    }
+}
+
+impl fmt::Debug for MatchFlag {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
 }
--- rzcli/src/node.rs
+++ rzcli/src/node.rs
@@ -13,6 +13,9 @@ use super::collate::*;
 
 // CLI Node trait.
 pub trait CliNode {
+    // Return inner.
+    fn inner(&self) -> &CliNodeInner;
+
     // Return ClI token string ref.
     fn token(&self) -> &str;
     
@@ -20,10 +23,10 @@ pub trait CliNode {
     fn collate(&self, input: &str) -> (MatchResult, MatchFlag);
 
     // Return help string ref.
-    fn help(&self) -> &str;
+    //fn help(&self) -> &str;
 
     // Return defun token.
-    fn defun_token(&self) -> &str;
+    //fn defun_token(&self) -> &str;
 }
 
 // Common field for CliNode
@@ -32,13 +35,13 @@ pub struct CliNodeInner {
     id: String,
 
     // Defun token.
-    defun_token: String,
+    defun: String,
 
     // Help string.
     help: String,
 
     // CLI token.
-    cli_token: String,
+    token: String,
 
     // Node vector is sorted.
     sorted: bool,
@@ -51,44 +54,106 @@ pub struct CliNodeInner {
 }
 
 impl CliNodeInner {
-    pub fn new(id: String, defun_token: String, help: String, cli_token: String) -> CliNodeInner {
+    pub fn new(id: String, defun: String, help: String, token: String) -> CliNodeInner {
         CliNodeInner {
             id: id,
-            defun_token: defun_token,
+            defun: defun,
             help: help,
-            cli_token: cli_token,
+            token: token,
             sorted: false,
             hidden: false,
             next: Vec::new(),
         }
     }
+
+    fn help(&self) -> &str {
+        &self.help
+    }
+
+    fn defun(&self) -> &str {
+        &self.defun
+    }
 }
 
 pub struct CliNodeKeyword {
+    inner: CliNodeInner,
+}
+
+impl CliNodeKeyword {
+    pub fn new(id: String, defun: String, help: String, token: String) -> CliNodeKeyword {
+        CliNodeKeyword {
+            inner: CliNodeInner::new(id, defun, help, token)
+        }
+    }
+}
+
+impl CliNode for CliNodeKeyword {
+    fn inner(&self) -> &CliNodeInner {
+        &self.inner
+    }
+
+    fn token(&self) -> &str {
+        &self.inner.token
+    }
+    
+    fn collate(&self, input: &str) -> (MatchResult, MatchFlag) {
+        if input == self.token() {
+            return (MatchResult::Success, MatchFlag::Full)
+        }
+
+        let t = &self.token()[0..input.len()];
+        if input == t {
+            return (MatchResult::Success, MatchFlag::Partial)
+        }
+
+        (MatchResult::Failure, MatchFlag::None)
+    }
 
 }
 
 pub struct CliNodeRange {
-
+    inner: CliNodeInner,
 }
 
 pub struct CliNodeIPv4Prefix {
-
+    inner: CliNodeInner,
 }
 
 pub struct CliNodeIPv4Address {
-
+    inner: CliNodeInner,
 }
     
 pub struct CliNodeIPv6Prefix {
-
+    inner: CliNodeInner,
 }
 
 pub struct CliNodeIPv6Address {
-
+    inner: CliNodeInner,
 }
     
 pub struct CliNodeWord {
+    inner: CliNodeInner,
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_node_keyword() {
+        let node = CliNodeKeyword::new(String::from("show"), String::from("show"),
+                                       String::from("help"), String::from("show"));
+        let (result, flag) = node.collate("show");
+        assert_eq!(result, MatchResult::Success);
+        assert_eq!(flag, MatchFlag::Full);
+
+        let (result, flag) = node.collate("sho");
+        assert_eq!(result, MatchResult::Success);
+        assert_eq!(flag, MatchFlag::Partial);
+
+        let (result, flag) = node.collate("shop");
+        assert_eq!(result, MatchResult::Failure);
+        assert_eq!(flag, MatchFlag::None);
+    }
 }
 
