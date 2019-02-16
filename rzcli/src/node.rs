b02da32de4c825cb3b53f345ba69bd23a0270bdf
Toshiaki Takada <toshiaki.takada@gmail.com>
2019-02-16 14:02:29 -0800

    Range node match.

--- rzcli/src/node.rs
+++ rzcli/src/node.rs
@@ -75,6 +75,8 @@ impl CliNodeInner {
     }
 }
 
+// CLI keyword node
+//   static literal
 pub struct CliNodeKeyword {
     inner: CliNodeInner,
 }
@@ -111,26 +113,71 @@ impl CliNode for CliNodeKeyword {
 
 }
 
+// CLI range node
+//   integer range to match numeric input.
 pub struct CliNodeRange {
     inner: CliNodeInner,
+    min: i64,
+    max: i64,
 }
 
+impl CliNodeRange {
+    pub fn new(id: String, defun: String, help: String,
+               min: i64, max: i64) -> CliNodeRange {
+        let token = format!("<{}-{}>", min, max);
+
+        CliNodeRange {
+            inner: CliNodeInner::new(id, defun, help, token),
+            min: min,
+            max: max,
+        }
+    }
+}
+
+impl CliNode for CliNodeRange {
+    fn inner(&self) -> &CliNodeInner {
+        &self.inner
+    }
+
+    fn token(&self) -> &str {
+        &self.inner.token
+    }
+    
+    fn collate(&self, input: &str) -> MatchResult {
+        let num = match input.parse::<i64>() {
+            Ok(num) => num,
+            Err(_err) => return MatchResult::Failure,
+        };
+
+        if num >= self.min && num <= self.max {
+            return MatchResult::Success(MatchFlag::Full)
+        }
+
+        MatchResult::Failure
+    }
+}
+
+//
 pub struct CliNodeIPv4Prefix {
     inner: CliNodeInner,
 }
 
+//
 pub struct CliNodeIPv4Address {
     inner: CliNodeInner,
 }
     
+//
 pub struct CliNodeIPv6Prefix {
     inner: CliNodeInner,
 }
 
+//
 pub struct CliNodeIPv6Address {
     inner: CliNodeInner,
 }
     
+//
 pub struct CliNodeWord {
     inner: CliNodeInner,
 }
@@ -151,6 +198,26 @@ mod tests {
         assert_eq!(result, MatchResult::Success(MatchFlag::Partial));
 
         let result = node.collate("shop");
+        assert_eq!(result, MatchResult::Failure);
+    }
+
+    #[test]
+    pub fn test_node_range() {
+        let node = CliNodeRange::new(String::from("range"), String::from("RANGE"),
+                                     String::from("help"), 100i64, 9999i64);
+
+        assert_eq!(node.token(), "<100-9999>");
+
+        let result = node.collate("100");
+        assert_eq!(result, MatchResult::Success(MatchFlag::Full));
+
+        let result = node.collate("99");
+        assert_eq!(result, MatchResult::Failure);
+
+        let result = node.collate("9999");
+        assert_eq!(result, MatchResult::Success(MatchFlag::Full));
+
+        let result = node.collate("10000");
         assert_eq!(result, MatchResult::Failure);
     }
 }
