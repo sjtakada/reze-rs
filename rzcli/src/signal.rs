327b2a5a05a622b40eaf6544ddc8367154ddf9ae
Toshiaki Takada <toshiaki.takada@gmail.com>
2019-04-09 19:53:18 -0700

    Cleanup.

--- rzcli/src/signal.rs
+++ rzcli/src/signal.rs
@@ -5,37 +5,32 @@
 // UNIX Signal.
 //
 
-use nix;
+use std::sync;
 use nix::sys::signal;
 
-use std::sync;
-
 static SIGTSTP_ONCE: sync::Once = sync::ONCE_INIT;
-
-use nix::sys::signal::SigHandler;
 
 pub fn ignore_sigtstp_handler() {
     SIGTSTP_ONCE.call_once(|| unsafe {
         let sa = signal::SigAction::new(
-            SigHandler::SigIgn,
+            signal::SigHandler::SigIgn,
             signal::SaFlags::empty(),
             signal::SigSet::empty(),
         );
         let result = signal::sigaction(signal::SIGTSTP, &sa);
 
-        match result {                                                                                                     
-            Ok(sa) if sa.handler() != SigHandler::SigIgn => {                                                              
-                let new_sa = signal::SigAction::new(                                                                       
-                    SigHandler::SigIgn,
-                    signal::SaFlags::empty(),                                                                              
-                    signal::SigSet::empty()                                                                                
-                );                                                                                                         
-                let _ = signal::sigaction(signal::SIGTSTP, &new_sa);                                                       
-            }                                                                                                              
-            _ => {                                                                                                         
-                
-            }                                                                                                              
-        }                                                                                                                  
+        match result {
+            Ok(sa) if sa.handler() != signal::SigHandler::SigIgn => {
+                let new_sa = signal::SigAction::new(
+                    signal::SigHandler::SigIgn,
+                    signal::SaFlags::empty(),
+                    signal::SigSet::empty()
+                );
+                let _ = signal::sigaction(signal::SIGTSTP, &new_sa);
+            }
+            _ => {
+            }
+        }
     });
 }
 
