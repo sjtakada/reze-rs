c21e0c83c7e439c620ae0a9e1f8ab827954a3946
Toshiaki Takada <toshiaki.takada@gmail.com>
2020-03-11 16:20:06 -0700

    Little update of show ip route output. (#18)

--- cli/scripts/cli_exec.rb
+++ cli/scripts/cli_exec.rb
@@ -3,11 +3,21 @@
 require 'json'
 require 'erb'
 
+# global definition.
+@rtype2code = {'Kernel' => 'K',
+               'Static' => 'S',
+               'Connected' => 'C',
+               'Eigrp' => 'D',
+               'Ospf' => 'O',
+               'Isis' => 'i',
+               'Rip' => 'R',
+               'Bgp' => 'B'}
+
 template_file = ARGV[0]
 json_str = STDIN.read
 
 @json = JSON.parse(json_str)
 
-template = ERB.new(File.read(template_file))
+template = ERB.new(File.read(template_file), nil, '-')
 puts template.result
 
