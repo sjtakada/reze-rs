d1b944ed7e8783b67cee825057ce848c408553bd
Toshiaki Takada <toshi@reze.rs>
2020-07-04 16:01:50 -0700

    cleanup.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -6,13 +6,13 @@
 //
 
 pub mod consts;
-pub mod error;
-pub mod event;
-pub mod timer;
-pub mod channel;
+//pub mod error;
+//pub mod event;
+//pub mod timer;
+//pub mod channel;
 pub mod method;
 
-pub mod uds_server;
+//pub mod uds_server;
 //pub mod uds_client;
 
 pub mod address_family;
